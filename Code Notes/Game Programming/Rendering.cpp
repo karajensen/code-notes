8a2467dc7912f966fde652daa34973393bb841bb
Kara Jensen <mail@karajensen.com>
2014-03-09 14:39:05 +0800

    updating rendering notes

--- Code Notes/Game Programming/Rendering.cpp
+++ Code Notes/Game Programming/Rendering.cpp
@@ -1,137 +1,89 @@
-﻿//////////////////////////////////////////////////////////////////////////////
-//RENDERING SYSTEMS
-//////////////////////////////////////////////////////////////////////////////
+﻿////////////////////////////////////////////////////////////////////////////////////////
+//RENDERING
+////////////////////////////////////////////////////////////////////////////////////////
 /*
 
 RAYTRACING      [PRE-RENDER]        Casts rays from light to scene to view
 RAYCASTING      [REALTIME-RENDER]   Casts rays from view to scene to light
 RASTERIZATION   [REALTIME-RENDER]   Projects to plane using proj matrix
 
-------------------------------------------------------------------------------
 FORWARD RAY-TRACING [RAY TRACING]
-------------------------------------------------------------------------------
-Rays traced from light into 3D scene
-Calculates new direction of rays when hitting surface
-Ideal for reflections/refractions
-Quality increases with more rays
-Disadvantage: hard to optimise, not possible in real time
-Disadvantage: lots of wasted rays that won't hit objects
+• Rays traced from light into 3D scene
+• Calculates new direction of rays when hitting surface
+• Ideal for reflections/refractions
+• Quality increases with more rays
+• Disadvantage: hard to optimise, not possible in real time
+• Disadvantage: lots of wasted rays that won't hit objects
 
-------------------------------------------------------------------------------
 BACKWARD RAY-TRACING [RAY CASTING]
-------------------------------------------------------------------------------
-Rays traced from camera into 3D scene
-Does not compute new direction a ray takes after reflections/refractions
-Ray sent for each pixel; number of rays depends on screen resolution
-Every pixel will always be shaded; less rays/faster than raytracing
+• Rays traced from camera into 3D scene
+• Does not compute new direction a ray takes after reflections/refractions
+• Ray sent for each pixel; number of rays depends on screen resolution
+• Every pixel will always be shaded; less rays/faster than raytracing
 
-------------------------------------------------------------------------------
 SCANLINE RENDERING [RASTERIZATION]
-------------------------------------------------------------------------------
-Algorithm for determining which models are visible. 
-It goes row by row for pixels rather than look at each polygon.
-Polygons are sorted by their y coordinate and then intersected with a scanline
-Polygons obstructed by others are discards as the line advances down the image
-Only vertices needed for current scanline are worked on, speeding up process
+• Algorithm for determining which models are visible. 
+• It goes row by row for pixels rather than look at each polygon.
+• Polygons are sorted by their y coordinate and then intersected with a scanline
+• Polygons obstructed by others are discards as the line advances down the image
+• Only vertices needed for current scanline are worked on, speeding up process
 
-------------------------------------------------------------------------------
 PATH TRACING [RAY TRACING]
-------------------------------------------------------------------------------
-Extends rays tracing where new rays are created and traced for any points of 
-intersection of objects. Takes into account indirect lighting/global illumination 
+• Extends rays tracing where new rays are created/traced for object intersection points
+• Takes into account indirect lighting/global illumination 
 
-//////////////////////////////////////////////////////////////////////////////  
-//REAL-TIME RENDERING / RASTERIZATION RENDER PIPELINES
-//////////////////////////////////////////////////////////////////////////////  
+////////////////////////////////////////////////////////////////////////////////////////
+RENDER PIPELINE
+////////////////////////////////////////////////////////////////////////////////////////
 
 TEXEL: One unit of a texture
 PIXEL: One unit of the screen
-FRAGMENT: Potential pixel. Each fragment has a pixel location, 
-          a depth value, color/texture values
+FRAGMENT: Potential pixel. Each fragment has a pixel location, a depth value, color/texture values
+HOMOGENEOUS CLIP SPACE: Screen space multiplied by 'w' component
 
-One pixel can be formed from many texels if image is shrunk
-One texel can be shared by many texels if image is enlarged
-Texels and pixels can map one-to-one when rendering to a quad
+01) FRUSTUM CULLING: Culls and creates list of meshes based on view frustum
+02) INPUT ASSEMBLER: Reads index/vertex data of each mesh
+03) VERTEX SHADER: Gets input vertex data, converts to homogenous clip space
+04) HULL SHADER: Inputs patch primitives, writes patch control points
+05) TESSELLATOR STAGE: Creates vertices and connectivity for tessellated lines or triangles.
+06) DOMAIN SHADER: Takes shaded control points and outputs vertices
+07) GEOMETRY SHADER: Inputs primitives optionally with adjacency information, then outputs different primitives
+08) STREAM-OUT: Writes Geometry shader output to buffer in memory
+09) BACKFACE CULLING: Face culling occurs depending on vert winding order
+10) CLIPPING: Clips any vertices outside the visible screen (If x,y is in range [-w,w] and z is in range [0,w] then okay)
+11) SCREEN MAPPING: Homogenous clip space transformed into screen space
+12) RASTERIZATION: 2D shape fitted into pixels
 
-=========================================================================
-SCENE RENDERING:
-=========================================================================
+    TRIANGLE TRAVERSAL:
+     • If the center of pixel is enclosed in shape, pixel is fully filled. 
+     • This creates a stepping, aliased effect. 
+     • 2D object may visibly shift in position during this process
 
-1) FRUSTUM CULLING
-List of scene meshes inside view frustum are compiled an culled based
-On the view frustum
+    FRAGMENT GENERATION:
+    • Every pixel covered by shape is a Fragment. 
+    • Fragment values are interpolated from the 2D triangles they're made up of
 
-2) VERTEX SHADER
-Positions transformed by wvp matrix into homogeneous clip space
-(screen space multiplied by 'w')
+13) PIXEL SHADING: Gets interpolated vertex data as fragments, outputs pixel colors
+14) OUTPUT MERGER: Gets shaded pixels from PS, does alpha blending and writes them back to the backbuffer.
+15) TRANSPARENCY: If translucent objects, render after all opaque meshes in back to front order
 
-3) GEOMETRY SHADER
-Any additional geometry is created in geometry shader
-
-4) BACKFACE CULLING
-Backface culling occurs depending on vert winding order
-
-5) CLIPPING
-Clips any vertices outside the screen
-If x,y is in range [-w,w] and z is in range [0,w] then okay
-
-6) SCREEN MAPPING
-Homogenous clip space transformed into screen space
-To get final coordinate, divide by w (which is screen space z)
-
------------------------------------------------------------------
-RASTERIZATION
-
-7) TRIANGLE TRAVERSAL
-2D shape fitted into pixels. If the center of pixel is enclosed in shape, 
-pixel is fully filled. This creates a stepping, aliased effect. 
-2D object may visibly shift in position during this process
-
-   PIXELS IN SCREEN:
-   (-0.5,-0.5)
-       o--o--o--o
-       |◄►|◄►|◄►|  ◄► = center of pixel (0,0) (1,0) (2,0)
-       o--o--o--o                       (0,1) (1,1) (2,1)             
-       |◄►|◄►|◄►|
-       o--o--o--o 
-             (1.5,2.5)
-
-8) FRAGMENT GENERATION
-Every pixel covered by shape is a Fragment. Fragment values are 
-interpolated from the 2D triangles they're made up of
------------------------------------------------------------------
-
-9) PIXEL SHADING
-Fragments sent to pixel shader and get texture/lighting applied
-
-10) COMPOSITION
-Depth/stencil buffers decide pixel's contribution to render target
-Alpha testing/blending is done if transparency
-
-=========================================================================
-RENDERING WITH TRANSPARENCY
-=========================================================================
-1) Render opaque in any order
-2) Render transparent objects back-to-front over top of opaque objects
-   Order important or else pixels that should be written to buffer are not
-
-=========================================================================
+==============================================================================
 FORWARD SHADING
-=========================================================================
-* Easy to render transparency
-* Expensive to do post effects
-* Are limited in amount of lights
+==============================================================================
+• Easy to render transparency
+• Expensive to do post effects
+• Are limited in amount of lights
 
 1) Render the scene with any lights that affect it to a single color buffer
 2) Render the scene again outputting any information needed for post effects
 3) Perform any post effects and present final image to the back buffer
 
-=========================================================================
+==============================================================================
 DEFERRED SHADING
-=========================================================================
-* Difficult to render transparecy
-* Cheap to do post effects
-* Can have many multiple lights
+==============================================================================
+• Difficult to render transparecy
+• Cheap to do post effects
+• Can have many multiple lights
 
 1) Render the scene once sending attributes to a G-buffer
    Attributes include: Normals, Depth, Diffuse, Specular, Ambient etc.
@@ -141,9 +93,9 @@ DEFERRED SHADING
    contribution of the light to the final pixel
 4) Perform any post effects with the ability to use the G-buffer
 
-//////////////////////////////////////////////////////////////////////////////  
+////////////////////////////////////////////////////////////////////////////////////////
 //GRAPHICS BUFFERS
-//////////////////////////////////////////////////////////////////////////////  
+////////////////////////////////////////////////////////////////////////////////////////
 
 FORMAT: Describes the layout of the resource’s data in memory
 PAGE FLIPPING: Double buffering to prevent tearing
@@ -179,9 +131,9 @@ ATTRIBUTE BUFFER
 RENDER TARGETS
 • Comprised of a color buffer and a depth/stencil buffer 
 
-//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //STENCILING
-//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 FOR MAIN SCENE RENDER TARGET
 1) Render scene as normal with without portal/mirror geometry
@@ -246,9 +198,9 @@ D3DSTENCILOP_DECR: Decrement the buffer entry, wraps to max if goes under 0
 gd3dDevice->SetRenderState(D3DRS_STENCILWRITEMASK, 0x0000ffff);
 NewValue = Value & WriteMask
 
-////////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////////////////////
 //TRANSFORMATIONS
-////////////////////////////////////////////////////////////////////////////// 
+////////////////////////////////////////////////////////////////////////////////////////
 
 ----------------
 |  YAW:    Y   |
@@ -357,9 +309,9 @@ To get final coordinate, divide by w (which is screen space z)
 | z' = (f/f-n)-(fn/(z(f-n))) = C+(D/z) |
  --------------------------------------
 
-///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //OPTIMISATION
-///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 FARPLANE CULLING
 • Any objects past the farplane are culled
@@ -390,9 +342,9 @@ OPTIMIZATION TIPS
 • Use half instead of float where strict precision isn't important
 • Render a depth-only pass before color pass to reduce pixel shader use
 
-///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //TERRAIN GENERATION
-///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 MIDPOINT DISPLACEMENT ALGORITHM
 • Start with basic four vertex shape and slip by adding vertex in middle
@@ -408,9 +360,9 @@ FAULT FORMATION ALGORITHM
 • Create another line and repeat, reducing d each pass
 • More passes = rougher terrain
 
-///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //PRIMITIVES
-///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 POINT LISTS:  .    .    . .   .
 LINE LIST: ._________________. 
@@ -439,4 +391,4 @@ POLYHEDRA: three dimensional object with flat faces and straight edges
 POLYTOPES: convex hulls of finite points sets (line segments, triangles, polyhedra)
 QUADRICS: spheres, cones, cylinders
 
-*/////////////////////////////////////////////////////////////////////////////
+*////////////////////////////////////////////////////////////////////////////////////////
