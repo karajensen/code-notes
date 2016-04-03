017495ddc11d6285a45f6f46ce9aa90302ceaebe
karajensen <mail@karajensen.com>
2016-04-03 13:53:19 +0800

    adding colour converter utility

---
+++ Utilities/ColourConverter/ColourConverter/ColourConverter.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ColourConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.blendValue = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b11 = new System.Windows.Forms.Button();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 66);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 66);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(171, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 66);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(157, 192);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 50;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 96);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "R: 0.0\r\nG: 0.0\r\nB: 0.0\r\nH: 0.0\r\nS: 0.0\r\nV: 0.0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(185, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 96);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R: 0.0\r\nG: 0.0\r\nB: 0.0\r\nH: 0.0\r\nS: 0.0\r\nV: 0.0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 96);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "R: 0.0\r\nG: 0.0\r\nB: 0.0\r\nH: 0.0\r\nS: 0.0\r\nV: 0.0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // blendValue
+            // 
+            this.blendValue.AutoSize = true;
+            this.blendValue.Location = new System.Drawing.Point(199, 228);
+            this.blendValue.Name = "blendValue";
+            this.blendValue.Size = new System.Drawing.Size(22, 13);
+            this.blendValue.TabIndex = 7;
+            this.blendValue.Text = "0.0";
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(50, 253);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(23, 23);
+            this.b1.TabIndex = 8;
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(79, 253);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(23, 23);
+            this.b2.TabIndex = 9;
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(108, 253);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(23, 23);
+            this.b3.TabIndex = 10;
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(138, 253);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(23, 23);
+            this.b4.TabIndex = 11;
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(167, 253);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(23, 23);
+            this.b5.TabIndex = 12;
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(196, 253);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(23, 23);
+            this.b6.TabIndex = 13;
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(226, 253);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(23, 23);
+            this.b7.TabIndex = 14;
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(255, 253);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(23, 23);
+            this.b8.TabIndex = 15;
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(284, 253);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(23, 23);
+            this.b9.TabIndex = 16;
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // b10
+            // 
+            this.b10.Location = new System.Drawing.Point(313, 253);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(23, 23);
+            this.b10.TabIndex = 17;
+            this.b10.UseVisualStyleBackColor = true;
+            // 
+            // b11
+            // 
+            this.b11.Location = new System.Drawing.Point(342, 253);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(23, 23);
+            this.b11.TabIndex = 18;
+            this.b11.UseVisualStyleBackColor = true;
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(171, 300);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(75, 23);
+            this.buttonRandom.TabIndex = 19;
+            this.buttonRandom.Text = "Random";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // ColourConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 344);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.blendValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ColourConverter";
+            this.Text = "Colour Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label blendValue;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button buttonRandom;
+    }
+}
+
---
+++ Utilities/ColourConverter/ColourConverter/ColourConverter.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ColourConverter : Form
+    {
+        private double m_blendValue = 0.0f;
+        private List<Button> m_blending = new List<Button>();
+        private Random m_random = new Random(0);
+
+        class RGB
+        {
+            public double Red = 0.0f;
+            public double Green = 0.0f;
+            public double Blue = 0.0f;
+        };
+
+        class HSV
+        {
+            public double Hue = 0.0f;
+            public double Saturation = 0.0f;
+            public double Value = 0.0f;
+        };
+
+        /// <summary>
+        /// Constructur
+        /// </summary>
+        public ColourConverter()
+        {
+            InitializeComponent();
+
+            colorDialog1.Color = Color.FromArgb(255, 128, 64);
+            colorDialog2.Color = Color.FromArgb(0, 128, 255);
+            button1.BackColor = colorDialog1.Color;
+            button2.BackColor = colorDialog2.Color;
+
+            m_blending.Add(b1);
+            m_blending.Add(b2);
+            m_blending.Add(b3);
+            m_blending.Add(b4);
+            m_blending.Add(b5);
+            m_blending.Add(b6);
+            m_blending.Add(b7);
+            m_blending.Add(b8);
+            m_blending.Add(b9);
+            m_blending.Add(b10);
+            m_blending.Add(b11);
+
+            SetBlendValue();
+            SetMiddleColour();
+        }
+
+        /// <summary>
+        /// On Button 1 click show colour dialog
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button1.BackColor = colorDialog1.Color;
+                SetMiddleColour();
+            }
+        }
+
+        /// <summary>
+        /// On Button 2 click show colour dialog
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+            {
+                button2.BackColor = colorDialog2.Color;
+                SetMiddleColour();
+            }
+        }
+
+        /// <summary>
+        /// On Track bar value change
+        /// </summary>
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            SetBlendValue();
+            SetMiddleColour();
+        }
+
+        /// <summary>
+        /// Converting RGB-HSV: http://www.poynton.com/PDFs/coloureq.pdf p15
+        /// Where rgb is [0,1]
+        /// </summary>
+        private HSV ConvertColour(RGB rgb)
+        {
+            HSV hsv = new HSV();
+
+            double min = Math.Min(Math.Min(rgb.Red, rgb.Blue), rgb.Green);
+            double max = Math.Max(Math.Max(rgb.Red, rgb.Blue), rgb.Green);
+            hsv.Saturation = max == 0.0f ? 0.0f : (max - min) / max;
+            hsv.Value = max;
+
+            double red = (max - rgb.Red) / (max - min);
+            double green = (max - rgb.Green) / (max - min);
+            double blue = (max - rgb.Blue) / (max - min);
+
+            if (hsv.Saturation != 0)
+            {
+                if (rgb.Red == max && rgb.Green == min)
+                {
+                    hsv.Hue = 5.0f + blue;
+                }
+                else if (rgb.Red == max && rgb.Green != min)
+                {
+                    hsv.Hue = 1.0f - green;
+                }
+                else if (rgb.Green == max && rgb.Blue == min)
+                {
+                    hsv.Hue = 1.0f + red;
+                }
+                else if (rgb.Green == max && rgb.Blue != min)
+                {
+                    hsv.Hue = 3.0f - blue;
+                }
+                else if (rgb.Red == max || rgb.Red == min)
+                {
+                    hsv.Hue = 3.0f + green;
+                }
+                else
+                {
+                    hsv.Hue = 5.0f - red;
+                }
+            }
+
+            hsv.Hue *= 60.0f;
+            return hsv;
+        }
+
+        /// <summary>
+        /// Converting HSV-RGB: http://www.poynton.com/PDFs/coloureq.pdf p15
+        /// Where h is [0,360] and sv is [0,1]
+        /// </summary>
+        private RGB ConvertColour(HSV hsv)
+        {
+            RGB rgb = new RGB();
+
+            double hex = hsv.Hue / 60.0f;
+            int primary = (int)Math.Floor(hex);
+            double secondary = hex - primary;
+            double a = (1.0f - hsv.Saturation) * hsv.Value;
+            double b = (1.0f - (hsv.Saturation * secondary)) * hsv.Value;
+            double c = (1.0f - (hsv.Saturation * (1.0f - secondary))) * hsv.Value;
+
+            switch(primary)
+            {
+            case 0:
+                rgb.Red = hsv.Value;
+                rgb.Green = c;
+                rgb.Blue = a;
+                break;
+            case 1:
+                rgb.Red = b;
+                rgb.Green = hsv.Value;
+                rgb.Blue = a;
+                break;
+            case 2:
+                rgb.Red = a;
+                rgb.Green = hsv.Value;
+                rgb.Blue = c;
+                break;
+            case 3:
+                rgb.Red = a;
+                rgb.Green = b;
+                rgb.Blue = hsv.Value;
+                break;
+            case 4:
+                rgb.Red = c;
+                rgb.Green = a;
+                rgb.Blue = hsv.Value;
+                break;
+            case 5:
+                rgb.Red = hsv.Value;
+                rgb.Green = a;
+                rgb.Blue = b;
+                break;
+            case 6:
+                rgb.Red = hsv.Value;
+                rgb.Green = c;
+                rgb.Blue = a;
+                break;
+            }
+
+            return rgb;
+        }
+
+        /// <summary>
+        /// Colour Blending: http://www.stuartdenman.com/improved-color-blending/
+        /// Where blend value is [0-1]
+        /// </summary>
+        private HSV BlendColour(HSV one, HSV two, double blendvalue)
+        {
+            HSV blend = new HSV();
+
+            double invBlendValue = 1.0f - blendvalue;
+            blend.Saturation = (one.Saturation * invBlendValue) + (two.Saturation * blendvalue);
+            blend.Value = (one.Value * invBlendValue) + (two.Value * blendvalue);
+
+            double hue1 = one.Hue;
+            double hue2 = two.Hue;
+            double difference = hue2 - hue1;
+
+            if (hue1 > hue2)
+            {
+                hue2 = one.Hue;
+                hue1 = two.Hue;
+
+                blendvalue = invBlendValue;
+                difference = -difference;
+            }
+
+            if(difference > 180)
+            {
+                hue1 += 360.0f;
+                blend.Hue = ((int)(hue1 + (blendvalue * (hue2 - hue1)))) % 360;
+            }
+            else
+            {
+                blend.Hue = hue1 + (blendvalue * difference);
+            }
+
+            return blend;
+        }
+
+        /// <summary>
+        /// Updates and sets the middle button colour
+        /// </summary>
+        private void SetMiddleColour()
+        {
+            Func<RGB, HSV, string> CreateColourText = (RGB rgb, HSV hsv) =>
+            {
+                return "R: " + rgb.Red.ToString("0.00") + "\n" +
+                       "G: " + rgb.Green.ToString("0.00") + "\n" +
+                       "B: " + rgb.Blue.ToString("0.00") + "\n" +
+                       "H: " + hsv.Hue.ToString("0.00") + "\n" +
+                       "S: " + hsv.Saturation.ToString("0.00") + "\n" +
+                       "V: " + hsv.Value.ToString("0.00");
+            };
+
+            RGB one = new RGB();
+            one.Red = (double)button1.BackColor.R / 255.0f;
+            one.Green = (double)button1.BackColor.G / 255.0f;
+            one.Blue = (double)button1.BackColor.B / 255.0f;
+            HSV oneHSV = ConvertColour(one);
+            label1.Text = CreateColourText(one, oneHSV);
+
+            RGB two = new RGB();
+            two.Red = (double)button2.BackColor.R / 255.0f;
+            two.Green = (double)button2.BackColor.G / 255.0f;
+            two.Blue = (double)button2.BackColor.B / 255.0f;
+            HSV twoHSV = ConvertColour(two);
+            label2.Text = CreateColourText(two, twoHSV);
+
+            int index = 0;
+            for (double i = 0.0; i < 1.0; i += 0.1)
+            {
+                HSV blendHSV = BlendColour(oneHSV, twoHSV, i);
+                RGB blend = ConvertColour(blendHSV);
+                var colour = Color.FromArgb((int)(blend.Red * 255.0f),
+                                            (int)(blend.Green * 255.0f),
+                                            (int)(blend.Blue * 255.0f));
+
+                if (m_blendValue <= i && m_blendValue > i - 0.1)
+                {
+                    button3.BackColor = colour;
+                    label3.Text = CreateColourText(blend, blendHSV);
+                }
+
+                m_blending[index].BackColor = colour;
+                ++index;
+            }
+        }
+
+        /// <summary>
+        /// Updates and sets the blend value
+        /// </summary>
+        private void SetBlendValue()
+        {
+            m_blendValue = (double)trackBar1.Value / 100.0f;
+            blendValue.Text = m_blendValue.ToString("0.0");
+        }
+
+        /// <summary>
+        /// On Random button click
+        /// </summary>
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = Color.FromArgb(m_random.Next(255), m_random.Next(255), m_random.Next(255));
+            colorDialog2.Color = Color.FromArgb(m_random.Next(255), m_random.Next(255), m_random.Next(255));
+            button1.BackColor = colorDialog1.Color;
+            button2.BackColor = colorDialog2.Color;
+            SetMiddleColour();
+        }
+    }
+}
