3dea22b057bbc5a8be48cb5f1c69d233be082d28
Kara Jensen <mail@karajensen.com>
2017-04-10 09:32:15 +1000

    Update Qt
    Notes for Qt

--- Code Notes/Languages/Qt.cpp
+++ Code Notes/Languages/Qt.cpp
@@ -1,100 +1,121 @@
-﻿////////////////////////////////////////////////////////////////////////////////////////////////////
-//QML
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+//QT
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 
-//• Model: Contains C++ data for a control, can have multiple views for the same data
-//• View : Displays the C++ data in Qt / QML in some way(eg.list, slider)
-//• QML uses javascript
+• Model: Contains C++ data for a control, can have multiple views for the same data
+• View: Displays the C++ data in Qt / QML in some way (eg.list, slider)
+• QML: Markup language for user interfaces made up of elements, uses Javascript for scripting
+• Module: Library components of Qt (eg. QtCore, QtGUI)
+
+*/
+//===================================================================================================
+// CONTAINERS
+//===================================================================================================
+
+// QSTRING
+QString myStr = "str";
+myStr.split(" ");                    // Returns QStringList of string split by spaces
+
+// QSTRINGLIST
+QStringList myList;                  // Inherits from QList<QString>, specifically for strings
+
+// QLIST
+QList<QString> myList;               // Fast index-based access, insertions and removals
+myList << "str1" << "str2";          // Allows streaming into container
+myList.join(" ")                     // Returns a combined string seperated by spaces
+
+QListIterator<QString> iter(myList);
+while(iter.hasNext()) 
+{
+    iter.next();
+}
+
+//===================================================================================================
+// QML
+//===================================================================================================
 
 import QtQuick 2.4
 import QtQuick.Controls 1.2
 import QtQuick.Layouts 1.0
 
-//--------------------------------------------------------------------------------------------
-// SHARED PROPERTIES
-//--------------------------------------------------------------------------------------------
-id: nameOfControl
-objectName: "UserName"                  // user defined name
-anchors.left: parent.left               // don't use with RowLayout, use Layout.fillWidth
-anchors.right: parent.right             // don't use with RowLayout, use Layout.fillWidth
-anchors.left: parent.left               // don't use with ColumnLayout, use Layout.fillHeight
-anchors.right: parent.right             // don't use with ColumnLayout, use Layout.fillHeight
-anchors.fill: parent                    // completely fills to parent
-anchors.margins: 1                      // adds margins between the anchor
-heigh : 30                              // avoid as easily ovewritten
-width: 30                               // avoid as easily ovewritten
-visible: true                           // whether control is visible
+Qt.quit()  // Quits the application
 
-/* Called when the object has been instantiated */
-Component.onCompleted:
-{
+//--------------------------------------------------------------------------------------------------
+// ELEMENT COMMON PROPERTIES
+//--------------------------------------------------------------------------------------------------
+id: nameOfControl                         // unique id of element, can be used to access it
+objectName: "UserName"                    // user defined name
+anchors.left: parent.left                 // don't use with RowLayout, use Layout.fillWidth
+anchors.right: parent.right               // don't use with RowLayout, use Layout.fillWidth
+anchors.left: parent.left                 // don't use with ColumnLayout, use Layout.fillHeight
+anchors.right: parent.right               // don't use with ColumnLayout, use Layout.fillHeight
+anchors.fill: parent                      // completely fills to parent
+anchors.margins: 1                        // adds margins between the anchor
+heigh : 30                                // avoid as easily ovewritten
+width: 30                                 // avoid as easily ovewritten
+visible: true                             // whether control is visible
+property var myProperty: true             // custom property
+property alias myProperty2: myProperty;   // alias for property
+    
+/* Called when the element has been instantiated */
+Component.onCompleted: {
 }
 
-/* Called when the object is destroyed */
-Component.onDestruction :
-{
+/* Called when the element is destroyed */
+Component.onDestruction: {
 }
 
 /* Called when the property 'color' has changed */
-onColorChanged:
-{
+onColorChanged: {
 }
 
-//--------------------------------------------------------------------------------------------
-// USER-DEFINED PROPERTIES
-//--------------------------------------------------------------------------------------------
-property var myProperty: true           // custom property
-property alias myProperty2: myProperty; // alias for property
-
-/* Callable function in QML */
-function myFunction(x, y)
-{
+/* Javascript custom function */
+function myFunction(x, y) {
     return x + y;
 }
 
-//--------------------------------------------------------------------------------------------
-// QML Components
-//--------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
+// ELEMENTS
+//--------------------------------------------------------------------------------------------------
 
 /* Aligns elements after each other in a single row */
-RowLayout
-{
+RowLayout {
     spacing: 5
 }
 
 /* Aligns elements after each other in a single column */
-ColumnLayout
-{
+ColumnLayout {
     spacing: 5
 }
 
 /* Aligns elements in a grid with n columns */
-GridLayout
-{
+GridLayout {
     columns: 3
     spacing: 5
 }
 
 /* Interactable area */
-MouseArea
-{
+MouseArea {
     acceptedButtons: Qt.AllButtons
     onPressed: {}
     onRelease: {}
-    onClicked:
-    {
+    onClicked: {
         var clickX = mouse.x;
         var clickY = mouse.y;
     }
 }
 
 /* Invisible Rectangle */
-Item
-{
+Item {
 }
 
 /* Visible Rectangle */
-Rectangle
-{
+Rectangle {
     color: "red"
+}
+
+/* Visible Text */
+Text {
+    text: "text"
 }
