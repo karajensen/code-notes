f02cf0a26b251f3b5358e58924743f3c5bd85eea
Kara Jensen <mail@karajensen.com>
2017-04-10 11:02:32 +1000

    update javascript

--- Code Notes/Languages/Javascript.cpp
+++ Code Notes/Languages/Javascript.cpp
@@ -15,75 +15,23 @@ typeof x != 'undefined'           // returns bool if true, can use null instead
 x === y                           // equal value and equal type
 x !== y                           // not equal value or not equal type
 
-// STRINGS
-str = "MyString\n";               // supports escape characters
-str = new String("MyString");     
-str.toUpperCase();                // Returns upper case of string
-str.toLowerCase();                // Returns lower case of string
-str.indexOf("substring");         // Returns first index of or -1 if not found
-str.length                        // Number of characters in string
-str.substr(0, 3);                 // Generate substring from index 0 to 3
-str.replace(/_[A-Za-z0-9]*/g,"")  // replace everything after _ with ""
-str.replace(str1, str2);          // returns new string
-str.split(",")                    // returns array of string without seperator
-                                     
-// ARRAYS
-myArray = new Array();
-myArray.length 
-myArray[0] = "myEntry";
-myArray["one"] = 2.0;             // creates new entry if key doesn't exist
-"one" in myArray                  // returns true if 'one' is a key in myArray
-
-// Remove key from array
-var index = myArray.indexOf(key);
-if (index > -1) {
-    myArray.splice(index, n);
-}
-
-// ENUMS
-MyEnum =
-{
-    ONE: 1,
-    TWO : 3,
-    THREE : 3
-}
-var myEnum = myVar.MyEnum.ONE
-
 // CONVERSIONS
 myString = myInt.toString();
 myString = 8 + "MyString";       // Auto converts: "8MyString"
 myInt = parseInt("8");           // Returns NaN if not a number
 
-// FUNCTIONS
-function MyFunction(myString, myArg)
-{
-    var localInt = 10;            // Local to function scope, without becomes global
-    return myArg + myint;         // All global variables avaliable
+// ENUMS
+MyEnum = {
+  ONE: 1,
+  TWO : 3,
+  THREE : 3
 }
-
-myArg = MyFunction("MyText", 5);
-MyFunction("MyText");            // any arguments to right not used become null
-MyFunction({a:"MyText", b:5});   // using named arguments
-
-// CLASSES
-// No constructors/destructors
-function MyClass(b) 
-{
-    this.myMember = b; 
-
-    this.MyMethod = function(a)
-    {
-        this.myMember = a;
-    }
-}
-var myObj = new MyClass(b);
+var myEnum = myVar.MyEnum.ONE
 
 // EXCEPTIONS
-try
-{
+try 
 }
-catch(ex)
-{
+catch(ex) 
     alert(ex.toString());
     throw ex; // rethrow same exception so console can log it
 }
@@ -107,7 +55,64 @@ for (i = 1; i <= 100; i++) { }
 do { } while (myBoolean)
 for (var key in myArray){ var x = myArray[key] } // safe way to iterate, using i++ may create key
 
+//================================================================================================
+// STRINGS
+//================================================================================================
+str = "MyString\n";               // supports escape characters
+str = new String("MyString");     
+str.toUpperCase();                // Returns upper case of string
+str.toLowerCase();                // Returns lower case of string
+str.indexOf("substring");         // Returns first index of or -1 if not found
+str.length                        // Number of characters in string
+str.substr(0, 3);                 // Generate substring from index 0 to 3
+str.replace(/_[A-Za-z0-9]*/g,"")  // replace everything after _ with ""
+str.replace(str1, str2);          // returns new string
+str.split(",")                    // returns array of string without seperator
+      
+//================================================================================================
+// ARRAYS
+//================================================================================================
+myArray = new Array();
+myArray.length 
+myArray[0] = "myEntry";
+myArray["one"] = 2.0;             // creates new entry if key doesn't exist
+"one" in myArray                  // returns true if 'one' is a key in myArray
+
+// Remove key from array
+var index = myArray.indexOf(key); // returns -1 if cannot find key
+if (index > -1) {
+    myArray.splice(index, n);    // number of keys to remove
+}
+
+//================================================================================================
+// FUNCTIONS
+//================================================================================================
+function MyFunction(myString, myArg) 
+    var localInt = 10;            // Local to function scope, without becomes global
+    return myArg + myint;         // All global variables avaliable
+}
+myArg = MyFunction("MyText", 5);
+MyFunction("MyText");            // any arguments to right not used become null
+MyFunction({a:"MyText", b:5});   // using named arguments
+
+//================================================================================================
+// CLASSES
+//================================================================================================
+// No constructors/destructors
+function MyClass(b) 
+{
+    this.myMember = b; 
+
+    this.MyMethod = function(a)
+    {
+        this.myMember = a;
+    }
+}
+var myObj = new MyClass(b);
+
+//================================================================================================
 // LIBRARIES
+//================================================================================================
 Math.max(a,b)
 Math.abs(a)
 Math.random()  // random number [0, 1)
