b2784f11d1d95c3f03a67350d5aef7f5e6c8b7fd
Kara Jensen <mail@karajensen.com>
2018-01-09 16:49:39 +1100

    Update C++ Error Handling.cpp

--- Code Notes/Languages/C++ Error Handling.cpp
+++ Code Notes/Languages/C++ Error Handling.cpp
@@ -93,12 +93,12 @@ void MyFn() noexcept; //Doesn't throw exceptions, optimizes: doesn't keep unwind
 //UNEXPECTED EXPECTION
 //If type wasn't explicitly thrown or on expected list: unexpected()->terminate()->abort()
 unexpected() //calls terminate()
-set_unexpected([](){}); //takes in void MyFunction()
+set_unexpected([](){ /** do something */ });
 
 //UNCAUGHT EXCEPTION
 //If type was known but not caught: terminate()->abort()
 terminate() //calls abort()
-set_terminate([](){}); //takes in void MyFunction()
+set_terminate([](){ /** do something */ });
  
 //EXCEPTIONS IN CONSTRUCTORS
 MyClass::MyClass()
@@ -134,16 +134,13 @@ SEH EXCEPTIONS:
 • Used for CPU exceptions such as access violation, illegal instruction, divide by zero
 • No exception guarantee; does not unwind the stack or call destructors
 • Each thread has its own SEH exception handler callback function
+• Using __try will add a new EXCEPTION_REGISTRATION to the list
 • When an exception occurs:
-    - linked list of EXCEPTION_REGISTRATION structures is walked one handles the exception
-    - using __try will add a new entry to the list on the stack
+    - linked list of EXCEPTION_REGISTRATION structures is walked util one handles the exception
     - default EXCEPTION_REGISTRATION is always called last
     - once handled, the list is walked again but with EH_UNWINDING (value 2) set in the exception flags
     - EH_UNWINDING gives the oppotunity to do any cleanup
 *****************************************************************************************************/
-static DWORD EH_UNWINDING = 0x00000002;
-static bool HANDLING_EXCEPTION = false;
-
 __try
 {
 }
@@ -153,14 +150,81 @@ __except(ExceptionFilter(GetExceptionCode(), GetExceptionInformation()))
 
 int ExceptionFilter(unsigned int code, struct _EXCEPTION_POINTERS* ep)
 {
+    static DWORD EH_UNWINDING = 0x00000002;
     if (ep->ExceptionRecord->ExceptionFlags & EH_UNWINDING) 
     {
         // Do any cleanup
     }
-    else if(!HANDLING_EXCEPTION)
+    else
     {
-        HANDLING_EXCEPTION = true; // Prevent multiple exceptions
-        // Do any handling     
+        auto info = InfoFromSE::information(code, ep);
     }  
     return EXCEPTION_CONTINUE_SEARCH; // Continue to the next handler
 }
+
+class InfoFromSE
+{
+public:
+   typedef unsigned int exception_code_t;
+
+   static const char* opDescription(const ULONG opcode)
+   {
+      switch(opcode) 
+      {
+      case 0: return "read";
+      case 1: return "write";
+      case 8: return "user-mode data execution prevention (DEP) violation";
+      default: return "unknown";
+      }
+   }
+
+   static const char* seDescription(const exception_code_t& code)
+   {
+      switch(code)
+      {
+         case EXCEPTION_ACCESS_VIOLATION:         return "EXCEPTION_ACCESS_VIOLATION"         ;
+         case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:    return "EXCEPTION_ARRAY_BOUNDS_EXCEEDED"    ;
+         case EXCEPTION_BREAKPOINT:               return "EXCEPTION_BREAKPOINT"               ;
+         case EXCEPTION_DATATYPE_MISALIGNMENT:    return "EXCEPTION_DATATYPE_MISALIGNMENT"    ;
+         case EXCEPTION_FLT_DENORMAL_OPERAND:     return "EXCEPTION_FLT_DENORMAL_OPERAND"     ;
+         case EXCEPTION_FLT_DIVIDE_BY_ZERO:       return "EXCEPTION_FLT_DIVIDE_BY_ZERO"       ;
+         case EXCEPTION_FLT_INEXACT_RESULT:       return "EXCEPTION_FLT_INEXACT_RESULT"       ;
+         case EXCEPTION_FLT_INVALID_OPERATION:    return "EXCEPTION_FLT_INVALID_OPERATION"    ;
+         case EXCEPTION_FLT_OVERFLOW:             return "EXCEPTION_FLT_OVERFLOW"             ;
+         case EXCEPTION_FLT_STACK_CHECK:          return "EXCEPTION_FLT_STACK_CHECK"          ;
+         case EXCEPTION_FLT_UNDERFLOW:            return "EXCEPTION_FLT_UNDERFLOW"            ;
+         case EXCEPTION_ILLEGAL_INSTRUCTION:      return "EXCEPTION_ILLEGAL_INSTRUCTION"      ;
+         case EXCEPTION_IN_PAGE_ERROR:            return "EXCEPTION_IN_PAGE_ERROR"            ;
+         case EXCEPTION_INT_DIVIDE_BY_ZERO:       return "EXCEPTION_INT_DIVIDE_BY_ZERO"       ;
+         case EXCEPTION_INT_OVERFLOW:             return "EXCEPTION_INT_OVERFLOW"             ;
+         case EXCEPTION_INVALID_DISPOSITION:      return "EXCEPTION_INVALID_DISPOSITION"      ;
+         case EXCEPTION_NONCONTINUABLE_EXCEPTION: return "EXCEPTION_NONCONTINUABLE_EXCEPTION" ;
+         case EXCEPTION_PRIV_INSTRUCTION:         return "EXCEPTION_PRIV_INSTRUCTION"         ;
+         case EXCEPTION_SINGLE_STEP:              return "EXCEPTION_SINGLE_STEP"              ;
+         case EXCEPTION_STACK_OVERFLOW:           return "EXCEPTION_STACK_OVERFLOW"           ;
+         default: return "UNKNOWN EXCEPTION" ;
+      }
+   }
+
+   static std::string information(exception_code_t code, struct _EXCEPTION_POINTERS* ep)
+   {
+      HMODULE hm;
+      ::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, static_cast<LPCTSTR>(ep->ExceptionRecord->ExceptionAddress), &hm);
+      MODULEINFO mi;
+      ::GetModuleInformation(::GetCurrentProcess(), hm, &mi, sizeof(mi));
+      char fn[MAX_PATH];
+      ::GetModuleFileNameExA(::GetCurrentProcess(), hm, fn, MAX_PATH);
+
+      std::ostringstream oss;
+      oss << "SE " << seDescription(code) << " at address 0x" << std::hex << ep->ExceptionRecord->ExceptionAddress << std::dec 
+         << " inside " << fn << " loaded at base address 0x" << std::hex << mi.lpBaseOfDll << "\n"; 
+
+      if (code == EXCEPTION_ACCESS_VIOLATION || code == EXCEPTION_IN_PAGE_ERROR))
+      {
+         oss << "Invalid operation: " << opDescription(ep->ExceptionRecord->ExceptionInformation[0]) 
+             << " at address 0x" << std::hex << ep->ExceptionRecord->ExceptionInformation[1] << std::dec << "\n";
+      }
+
+      return oss.str();
+   }
+};
