b4ff2d1b36b3235c6c576525a1060b54e5e873d6
Kara Jensen <mail@karajensen.com>
2018-02-07 09:41:32 +1100

    Update Qt.cpp

--- Code Notes/Languages/Qt.cpp
+++ Code Notes/Languages/Qt.cpp
@@ -1,10 +1,10 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//QT
-////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//QT OVERVIEW
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/**************************************************************************************************
-
+/*************************************************************************************************************
 QT MODULES
+**************************************************************************************************************
 QtCore               Core non-graphical classes used by other modules
 QtGUI                Base classes for graphical user interface (GUI) components. Includes OpenGL
 QtMultimedia         Classes for audio, video, radio and camera functionality
@@ -19,24 +19,33 @@ QtSQL                Classes for database integration using SQL
 QtTest               Classes for unit testing Qt applications and libraries
 QtWidgets            Classes to extend Qt GUI with C++ widgets
 
-***************************************************************************************************
-
+**************************************************************************************************************
 QT LICENSING
-Qt for Application Development: 
+**************************************************************************************************************
+GENERAL PUBLIC LICENSE (GPU):
+• Requires releasing the source code of your application if distributing the open source software 
+• All portions of the project must be under GPU as well
+
+LESSER GENERAL PUBLIC LICENSE (LGPU):
+• Allows distributing the open source software with your application without releasing the application source
+• LGPU open source software must be re-distributed as LGPU, other portions of project may have different licences
+
+QT FOR APPLICATION DEVELOPMENT: 
   • Cross platform development for desktop and mobile
   • Can be licensed under commercial and open source licenses (GPU / LGPU)
   • Static linking requires GPU licence and all project code public
   • Dynamic linking can use LGPU licence to keep parts of it private
-  • Some modules GPU only: Qt Data Visualization / Qt Charts / Tooling (if modifications are done)
-Qt for Device Creation: 
+  • Some modules GPU only: QtDataVisualization / QtCharts / Tooling (if modifications are done)
+  
+QT FOR DEVICE CREATION: 
   • Used with embedded devices
   • Only under commercial licence to allow integration with proprietary code 
   
-**************************************************************************************************/
+**************************************************************************************************************/
 
-//===================================================================================================
-// Qt Components
-//===================================================================================================
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//QT COMPONENTS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // QString
 QString str = "str";
@@ -55,9 +64,25 @@ lst << 1 << 2;                          // Allows streaming into container
 QListIterator<int> iter(myList);
 while(iter.hasNext()) { iter.next(); }
 
-//===================================================================================================
-// Qt Modeling Language (QML)
-//===================================================================================================
+//==========================================================================================================
+//FILE SYSTEM
+//==========================================================================================================
+
+// Read from a file, no need to close it
+QFile file("myFile.txt");
+if(file.open(QIODevice::ReadOnly))
+{
+    QTextStream stream(&file);
+    QString line = stream.readLine(); // read the next line
+    while(!line.isNull())
+    {
+        line = stream.readLine();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//QT MODELING LANGUAGE (QML)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //• QML: Markup language for user interfaces made up of elements, uses Javascript for scripting
 
 import QtQuick 2.4
@@ -66,9 +91,9 @@ import QtQuick.Layouts 1.0
 
 Qt.quit()  // Quits the application
 
-//--------------------------------------------------------------------------------------------------
-// QML PROPERTIES
-//--------------------------------------------------------------------------------------------------
+//==========================================================================================================
+//QML PROPERTIES
+//==========================================================================================================
 id: nameOfControl                         // unique id of element, can be used to access it
 objectName: "UserName"                    // user defined name
 anchors.left: parent.left                 // don't use with RowLayout, use Layout.fillWidth
@@ -100,9 +125,9 @@ function myFunction(x, y) {
     return x + y;
 }
 
-//--------------------------------------------------------------------------------------------------
-// QML ELEMENTS
-//--------------------------------------------------------------------------------------------------
+//==========================================================================================================
+//QML ELEMENTS
+//==========================================================================================================
 
 /* Aligns elements after each other in a single row */
 RowLayout {
@@ -145,26 +170,13 @@ Text {
     text: "text"
 }
 
-//===================================================================================================
-// MODELS / VIEWS
-//===================================================================================================
-//• Model: Contains the data and its structure
-//• View: A container that displays the data. The view might display the data in a list or a grid.
-//• Delegate: Determines how each element of data should appear in the view. Can also access each element.
-//• Role: Used to access different attributes of a data element in the model
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//QT MODELS / VIEWS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//===================================================================================================
-// FILE SYSTEM
-//===================================================================================================
-
-// Read from a file, no need to close it
-QFile file("myFile.txt");
-if(file.open(QIODevice::ReadOnly))
-{
-    QTextStream stream(&file);
-    QString line = stream.readLine(); // read the next line
-    while(!line.isNull())
-    {
-        line = stream.readLine();
-    }
-}
+/*************************************************************************************************************
+• Model: Contains the data and its structure
+• View: A container that displays the data. The view might display the data in a list or a grid.
+• Delegate: Determines how each element of data should appear in the view. Can also access each element.
+• Role: Used to access different attributes of a data element in the model
+**************************************************************************************************************/
