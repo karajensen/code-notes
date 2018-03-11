50197b69559969dc1a722c022722afb2afca4eb5
Kara Jensen <mail@karajensen.com>
2018-03-11 17:18:33 +1100

    Update Qt.cpp

--- Code Notes/Languages/Qt.cpp
+++ Code Notes/Languages/Qt.cpp
@@ -64,14 +64,22 @@ LGPLv2.1 vs. LGPLv3
 **************************************************************************************************************/
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// QT COMPONENTS
+// QT FRAMEWORK
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QTimer timer;
+    timer.setInterval(10);
+    QObject::connect(&timer, &QTimer::timeout, []() { /*tick*/ });
+    timer.start();
     app.exec(); // Start the event loop
 }
+
+//===============================================================================================================
+// QT COMPONENTS
+//===============================================================================================================
 
 // QString
 QString str = "str";
@@ -91,7 +99,7 @@ QListIterator<int> iter(myList);
 while(iter.hasNext()) { iter.next(); }
 
 //===============================================================================================================
-// WIDGETS
+// QT WIDGETS
 //===============================================================================================================
 
 // QWidget
@@ -120,7 +128,7 @@ slider.setValue(value);
 QObject::connect(slider, SIGNAL(valueChanged(int)), &app, SLOT(myFn(int)));
 
 //===============================================================================================================
-// LAYOUTS
+// QT LAYOUTS
 //===============================================================================================================
 
 // QHBoxLayout: lays out widgets horizontally from left to right
@@ -145,6 +153,104 @@ if(file.open(QIODevice::ReadOnly))
     {
         line = stream.readLine();
     }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// QT MODELING LANGUAGE (QML)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//• QML: Markup language for user interfaces made up of elements, uses Javascript for scripting
+
+import QtQuick 2.4
+import QtQuick.Controls 1.2
+import QtQuick.Layouts 1.0
+
+Qt.quit()  // Quits the application
+
+//===============================================================================================================
+// QML PROPERTIES
+//===============================================================================================================
+id: nameOfControl                         // unique id of element, can be used to access it
+objectName: "UserName"                    // user defined name
+anchors.left: parent.left                 // don't use with RowLayout, use Layout.fillWidth
+anchors.right: parent.right               // don't use with RowLayout, use Layout.fillWidth
+anchors.left: parent.left                 // don't use with ColumnLayout, use Layout.fillHeight
+anchors.right: parent.right               // don't use with ColumnLayout, use Layout.fillHeight
+anchors.fill: parent                      // completely fills to parent
+anchors.margins: 1                        // adds margins between the anchor
+heigh: 30                                 // avoid as easily ovewritten
+width: 30                                 // avoid as easily ovewritten
+visible: true                             // whether control is visible
+property var myProperty: true             // custom property
+property alias myProperty2: myProperty;   // alias for property
+    
+/* Called when the element has been instantiated */
+Component.onCompleted: {
+}
+
+/* Called when the element is destroyed */
+Component.onDestruction: {
+}
+
+/* Called when the property 'color' has changed */
+onColorChanged: {
+}
+
+/* Javascript custom function */
+function myFunction(x, y) {
+    return x + y;
+}
+
+//===============================================================================================================
+// QML COMPONENTS
+//===============================================================================================================
+
+/* Interactable area */
+MouseArea {
+    acceptedButtons: Qt.AllButtons
+    onPressed: {}
+    onRelease: {}
+    onClicked: {
+        var clickX = mouse.x;
+        var clickY = mouse.y;
+    }
+}
+
+/* Invisible Rectangle */
+Item {
+}
+
+//===============================================================================================================
+// QML LAYOUTS
+//===============================================================================================================
+
+/* Aligns elements after each other in a single row */
+RowLayout {
+    spacing: 5
+}
+
+/* Aligns elements after each other in a single column */
+ColumnLayout {
+    spacing: 5
+}
+
+/* Aligns elements in a grid with n columns */
+GridLayout {
+    columns: 3
+    spacing: 5
+}
+
+//===============================================================================================================
+// QML WIDGETS
+//===============================================================================================================
+
+/* Visible Rectangle */
+Rectangle {
+    color: "red"
+}
+
+/* Visible Text */
+Text {
+    text: "text"
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -196,96 +302,6 @@ public slots:
 private:
     MyEnum m_enum;
 };
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// QT MODELING LANGUAGE (QML)
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//• QML: Markup language for user interfaces made up of elements, uses Javascript for scripting
-
-import QtQuick 2.4
-import QtQuick.Controls 1.2
-import QtQuick.Layouts 1.0
-
-Qt.quit()  // Quits the application
-
-//===============================================================================================================
-// QML PROPERTIES
-//===============================================================================================================
-id: nameOfControl                         // unique id of element, can be used to access it
-objectName: "UserName"                    // user defined name
-anchors.left: parent.left                 // don't use with RowLayout, use Layout.fillWidth
-anchors.right: parent.right               // don't use with RowLayout, use Layout.fillWidth
-anchors.left: parent.left                 // don't use with ColumnLayout, use Layout.fillHeight
-anchors.right: parent.right               // don't use with ColumnLayout, use Layout.fillHeight
-anchors.fill: parent                      // completely fills to parent
-anchors.margins: 1                        // adds margins between the anchor
-heigh: 30                                 // avoid as easily ovewritten
-width: 30                                 // avoid as easily ovewritten
-visible: true                             // whether control is visible
-property var myProperty: true             // custom property
-property alias myProperty2: myProperty;   // alias for property
-    
-/* Called when the element has been instantiated */
-Component.onCompleted: {
-}
-
-/* Called when the element is destroyed */
-Component.onDestruction: {
-}
-
-/* Called when the property 'color' has changed */
-onColorChanged: {
-}
-
-/* Javascript custom function */
-function myFunction(x, y) {
-    return x + y;
-}
-
-//===============================================================================================================
-// QML ELEMENTS
-//===============================================================================================================
-
-/* Aligns elements after each other in a single row */
-RowLayout {
-    spacing: 5
-}
-
-/* Aligns elements after each other in a single column */
-ColumnLayout {
-    spacing: 5
-}
-
-/* Aligns elements in a grid with n columns */
-GridLayout {
-    columns: 3
-    spacing: 5
-}
-
-/* Interactable area */
-MouseArea {
-    acceptedButtons: Qt.AllButtons
-    onPressed: {}
-    onRelease: {}
-    onClicked: {
-        var clickX = mouse.x;
-        var clickY = mouse.y;
-    }
-}
-
-/* Invisible Rectangle */
-Item {
-}
-
-/* Visible Rectangle */
-Rectangle {
-    color: "red"
-}
-
-/* Visible Text */
-Text {
-    text: "text"
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // QT MODELS / VIEWS
