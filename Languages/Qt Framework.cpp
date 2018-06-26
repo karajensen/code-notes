565926b84862ef1a662d807cd3ae4884adb4511d
Kara Jensen <mail@karajensen.com>
2018-06-26 20:32:30 +0200

    Update Qt Framework.cpp

--- Languages/Qt Framework.cpp
+++ Languages/Qt Framework.cpp
@@ -147,9 +147,9 @@ metaObj.indexOfEnumerator(name) // Get index of enum from name, or -1 if not fou
 QMetaEnum metaEnum(QMetaEnum::fromType<MyClass::MyEnum>());
 metaEnum.valueToKey(MyClass::ONE, &isValid) // Returns const char* or null if not found, optional bool
 metaEnum.keyToValue("ONE", &isValid) // Returns int or -1 if not found, optional bool
-metaEnum.key(MyClass::ONE) // Returns const char* key from int index, or null if doesn't exist
+metaEnum.key(index) // Returns const char* key from int index, or null if doesn't exist
 metaEnum.keyCount() // Returns number of keys
-
+metaEnum.value(index) // Returns value with the given index, or returns -1 if none found
 
 // OBJECT TYPE INFO
 // Macro must be outside all namespaces, placed after class in .h
