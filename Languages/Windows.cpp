225b471b6173db31063d42494b3cfb33126b5b5d
Kara Jensen <mail@karajensen.com>
2018-07-04 21:50:59 +0200

    Update Windows.cpp

--- Languages/Windows.cpp
+++ Languages/Windows.cpp
@@ -10,3 +10,12 @@ wchar_t exePath[MAX_PATH];
 /** Get the mouse position */
 POINT cursor;
 ::GetCursorPos(&cursor);
+
+/** Get short path from an existing file */
+auto length = GetShortPathNameW(longWPath.c_str(), NULL, 0);
+wchar_t* buffer = new wchar_t[length];
+length = GetShortPathNameW(longWPath.c_str(), buffer, length); 
+if(length == 0){ /*show error*/ }
+std::wstring result(buffer);
+delete [] buffer;
+
