bc7ab36e68474e39eea5fc6f14ea9415cd1e5d58
Kara Jensen <mail@karajensen.com>
2018-07-04 21:57:24 +0200

    Update Windows.cpp

--- Languages/Windows.cpp
+++ Languages/Windows.cpp
@@ -19,3 +19,23 @@ if(length == 0){ /*show error*/ }
 std::wstring result(buffer);
 delete [] buffer;
 
+/** Get last error as string */
+DWORD error = GetLastError();
+if (error)
+{
+    LPVOID lpMsgBuf;
+    DWORD bufLen = FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS, 
+        NULL, error,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf, 0, NULL);
+  
+    if (bufLen)
+    {
+      LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
+      std::string result(lpMsgStr, lpMsgStr+bufLen);
+      LocalFree(lpMsgBuf);
+    }
+}
