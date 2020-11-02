17282358c679d0ef08de2014c9788aa42a502f3e
Kara Jensen <mail@karajensen.com>
2020-11-02 10:49:00 +0100

    Update C++ Memory.cpp

--- Languages/C++ Memory.cpp
+++ Languages/C++ Memory.cpp
@@ -57,24 +57,24 @@ myPointer->member;               // access member of object
 (*myPointer)->member;            // access pointer to pointer
 delete myPointer;                // frees the block of memory
 
-//POINTER TO ARRAYS
+// POINTER TO ARRAYS
 int* myArray = new int[SIZE];
 delete [] myArray;
 *(myArray+3) /*or*/ myArray[3] /*or*/ 3[myArray]
 
-//POINTER TO ARRAY OF POLYMORPHIC TYPES
-//Avoid as pointer arithmatic on base class arrays of derived objects
-//pointer arithmatic will use sizeof(Base) not sizeof(Derived)
+// POINTER TO ARRAY OF POLYMORPHIC TYPES
+// Avoid as pointer arithmatic on base class arrays of derived objects
+// pointer arithmatic will use sizeof(Base) not sizeof(Derived)
 Base* myArray = new Derived[2];
 myArray[1] = *(myArray+1)        // BAD
 delete [] myArray                // BAD, also uses pointer arithmatic
 
-//ARRAY-TO-POINTER DECAY RULE
+// ARRAY-TO-POINTER DECAY RULE
 const char myArray[] = "Array";       // note not the same type as const char*
 const char* pMyArray = myArray;       // character array decays to pointer
 const char rMyArray (&)[5] = myArray; // actual type of array
 
-//DISPLAY MEMORY LOCATION
+// DISPLAY MEMORY LOCATION
 std::stringstream ss;
 ss << std::hex << static_cast<void*>(&myVariable)
 
@@ -82,26 +82,26 @@ ss << std::hex << static_cast<void*>(&myVariable)
 // FUNCTION OBJECTS
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//FUNCTION ADDRESSES
+// FUNCTION ADDRESSES
 &MyFunction;                                 // address of non-class function
 (void(*)(double))&MyFunction;                // address of non-class overloaded function
 &MyClass::MyMethod                           // address of class method
 (void(MyClass::*)(double))&MyClass::MyMethod // address of class overloaded function
 
-//FUNCTION-TO-POINTER DECAY RULE
+// FUNCTION-TO-POINTER DECAY RULE
 void MyFunction(int);                 // Not the same type as void(*)(int)
 void(*pMyFunction)(int) = MyFunction; // function decays to pointer
 void(&rMyFunction)(int) = MyFunction; // actual type of function
 
-//POINTER-TO-FUNCTION
+// POINTER-TO-FUNCTION
 typedef bool(*MyFunctionPtr)(int, double); /*or*/ 
 using MyFunctionPtr = bool(*)(int, double);
 MyFunctionPtr myFunction = &MyFunction;
 myFunction(5, 1.0);
 
-//POINTER-TO-MEMBER FUNCTION
-//Cannot cast to pointer-to-function as can be much larger storing inheritance information
-//Cannot take address of contructor
+// POINTER-TO-MEMBER FUNCTION
+// Cannot cast to pointer-to-function as can be much larger storing inheritance information
+// Cannot take address of contructor
 class MyClass
 {
 public:
@@ -115,42 +115,50 @@ m_methodFn = &MyClass::MyMethod;
 (myObject.*m_methodFn)();
 (myObjectPtr->*m_methodFn)();
 
-//FUNCTOR
-//class with operator()
+// FUNCTOR
+// class with operator()
 struct MyFunctor
 {
     bool operator()(int x) { return x > 0; }
 }
 
-//STD::FUNCTION
-//Allows all function objects to be stored in single type
-//Slower and bigger than using actual function object types (lambda, functors, pointers)
-std::function<double(int)> myFn = [](int x){ return x+2.0; }
-std::function<double(int)> myFn = &MyFunction;
-std::function<double(int)> myFn = myLambda; // Copies the lambda
-std::function<double(int)> myFn = std::ref(myLambda); // Assigns lambda without copy
+// STD::FUNCTION
+// Allows all function objects to be stored in single type
+// Slower and bigger than using actual function object types (lambda, functors, pointers)
+std::function<double(int)> fn = [](int x){ return x+2.0; }
+std::function<double(int)> fn = &MyFunction;
+std::function<double(int)> fn = myLambda; // Copies the lambda
+std::function<double(int)> fn = std::ref(myLambda); // Assigns lambda without copy
 typedef double(MyFnType)(int); // Typedef for function return/params
 typedef std::function<MyFnType> MyFn; // Typedef for std::function
 
-//LAMBDAS
-//If inside a class, may become friends to access/capture internals
-//Lamda creates a closure object which holds captured vars, byval captured 'const' unless 'mutable' used
-auto myLambda = [](int x)->float {}      // specify return type, only need if multilined
-auto myLambda = [&var](int x){}          // capture only myVar by reference
-auto myLambda = [=var](int x){}          // capture only myVar by value
-auto myLambda = [var](int x){}           // capture only myVar by value, doesn't work on member vars
-auto myLambda = [&](int x){}             // capture all by ref
-auto myLambda = [=](int x){}             // capture all non-static local variables (including this) by-val
-auto myLambda = [var] mutable (int x){}  // capture by-val but allow modifying the values
+// LAMBDAS
+// If inside a class, may become friends to access/capture internals
+// Implicitly constexpr where possible (c++17)
+// Lamda creates a closure object which holds captured vars, byval captured 'const' unless 'mutable' used
+auto fn = []{}                             // Only need () with arguments, only need -> if multilined
+auto fn = [](int x) ->float {}             // specify return type
+auto fn = [&y](int x){}                    // capture only y by reference
+auto fn = [&y = as_const(y)](int x) {}     // same as &y but const
+auto fn = [=y](int x){}                    // capture only y by value, = optional, doesn't work on class members
+auto fn = [y] mutable (int x){}            // capture by-val but allow modifying the values
+auto fn = [&](int x){}                     // capture all by ref
+auto fn = [=](int x){}                     // capture all non-static local variables (including this) by-val
+auto fn = [](int x) constexpr {}           // Forces constexpr with error if not
+constexpr auto fn = [](int x) constexpr {} // Make init of fn also constexpr
 
-//LAMBDA INIT/GENERALISED CAPTURE
-//Allows creation of variables inside the closure
-auto myLambda = [var = myVar](){}           // create var and copy-assign to it, works on member vars
-auto myLambda = [var = MyClass()](){}       // create var of type MyClass
-auto myLambda = [var = std::move(obj)](){}  // move obj to be only used inside closure
-auto myLambda = [var = std::move(vec)](){}  // move vector of objects to use inside closure
+// LAMBDA CAPTURE THIS
+auto fn = [this](int x) {}                 // Capture this pointer by-value
+auto fn = [*this](int x) {}                // Same as self = *this, but without having to do self.value
+auto fn = [this, *this](int x) {}          // Error
 
-//LAMBDA RECURSION
+// LAMBDA GENERALISED CAPTURE
+// Allows creation of variables inside the closure
+auto fn = [var = myVar](){}           // create var and copy-assign to it, works on member vars
+auto fn = [var = MyClass()](){}       // create var of type MyClass
+auto fn = [var = std::move(obj)](){}  // move obj to be only used inside closure
+
+// LAMBDA RECURSION
 std::function<int(int)> fn;
 fn = [&fn](int value)->int 
 {
@@ -162,37 +170,37 @@ fn = [&fn](int value)->int
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Creates functor by capturing address and variables (by-ref)
 
-using namespace std::placeholders; //for _1, _2..
+using namespace std::placeholders; // for _1, _2..
 
-//SETTING ALL ARGUMENTS
+// SETTING ALL ARGUMENTS
 auto fullFn = std::bind(&MyFunction, 1, 10.0, 2.0);
 fullFn();
 
-//FULL BINDING
-auto fullFn = std::bind(&MyFunction, _1, _2, _3); //_n maps to argument given
-fullFn(1,10.0,2.0); //use same as MyFunction, passes by-val default
-fullFn(1,10.0,std::cref(myFloat)); //pass in argument by-ref
+// FULL BINDING
+auto fullFn = std::bind(&MyFunction, _1, _2, _3); // _n maps to argument given
+fullFn(1,10.0,2.0); // use same as MyFunction, passes by-val default
+fullFn(1,10.0,std::cref(myFloat)); // pass in argument by-ref
 
-//PARTIALLY BINDING
-auto partialFn = std::bind(&MyFunction, _1, 10.0, _2); //map only two arguments
-partialFn(1,2.0); //only requires two arguments, middle is auto given
+// PARTIALLY BINDING
+auto partialFn = std::bind(&MyFunction, _1, 10.0, _2); // map only two arguments
+partialFn(1,2.0); // only requires two arguments, middle is auto given
 
-//CHANGING ARGUMENT ORDER
-auto orderChangedFn = std::bind(&MyFunction, _3, _1, _2); //rearranges order of arguments
-orderChangedFn(1,10.0,2.0); //becomes MyFunction(2.0,1,10.0)
+// CHANGING ARGUMENT ORDER
+auto orderChangedFn = std::bind(&MyFunction, _3, _1, _2); // rearranges order of arguments
+orderChangedFn(1,10.0,2.0); // becomes MyFunction(2.0,1,10.0)
 
-//BINDING CLASS FUNCTIONS
-auto memberFn = std::bind(&MyClass::MyMethod, _1, _2); //_1 is always object
+// BINDING CLASS FUNCTIONS
+auto memberFn = std::bind(&MyClass::MyMethod, _1, _2); // _1 is always object
 auto memberFn = std::bind(&MyClass::MyMethod, this, _1);
 memberFn(&myObject, 2);
 memberFn(2) // object is bound with method
 
-//BINDING OVERLOADED FUNCTIONS
+// BINDING OVERLOADED FUNCTIONS
 void MyClass::MyMethod(int x);
 void MyClass::MyMethod(double x);
 std::bind((void(MyClass::*)(double))&MyClass::MyMethod, this, _1);
 
-//BINDING LAMBDAS
+// BINDING LAMBDAS
 std::bind([](int x){}, std::move(myInt)); // move construct myVar
 std::bind([](int x){}, myInt); // copy construct myVar
 
@@ -257,18 +265,18 @@ SMART POINTER DESTRUCTION
 **************************************************************************************************************/
 
 *myPtr
-if(myPtr) /*or*/ if(myPtr.get())              // true if valid, false if null
-std::dynamic_pointer_cast<MyClass>(ptr);      // returns cast or null from base to derived class
+if(myPtr) /*or*/ if(myPtr.get())                 // true if valid, false if null
+std::dynamic_pointer_cast<MyClass>(ptr);         // returns cast or null from base to derived class
 std::static_pointer_cast<MyClass>(ptr);
 
-//UNIQUE POINTER
+// UNIQUE POINTER
 auto unique(std::make_unique<MyClass>());        // can't use custom deleter or {}
 std::unique_ptr<MyClass> unique(new MyClass());
 std::unique_ptr<int[]> p(new int[10]);           // arrays only supported by unique_ptr, calls delete[]
 unique.reset()                                   // releases and deletes the memory
 unique.release();                                // releases and doesn't delete the memory, returns a raw pointer
 
-//SHARED POINTER
+// SHARED POINTER
 auto shared(std::make_shared<MyClass>());        // creates a new control block, can't use custom deleter or {}
 std::shared_ptr<MyClass> shared(new MyClass());  // creates new control block
 std::shared_ptr<MyClass> shared(unique);         // creates new control block, unique_ptr becomes null
@@ -279,24 +287,24 @@ shared.swap(shared2)                             // swap what the shared_ptr loo
 shared.unique()                                  // returns true if ref count = 1, false otherwise
 shared.reset()                                   // decrements ref count
 
-//WEAK POINTER
+// WEAK POINTER
 std::weak_ptr<MyClass> weak(shared);             // observing a shared pointer control block
 weak.use_count()                                 // get the current count
 weak.lock()                                      // returns a shared_ptr, if expired then null
 weak.expired()                                   // returns true if object has been deleted, false if okay
 
-//AUTO POINTER
+// AUTO POINTER
 auto_ptr<double> ap(new double) //only use for new
 *ap = 3.2;
 
-//CREATING CUSTOM DELETERS
-//Without a deleter, plain 'delete' is implicitely called
-//Deleter becomes part of type for unique_ptr
+// CREATING CUSTOM DELETERS
+// Without a deleter, plain 'delete' is implicitely called
+// Deleter becomes part of type for unique_ptr
 auto Release = [](MyClass* obj){ obj->Release(); }
 std::shared_ptr<MyClass> shared(&myObject, Release);
 std::unique_ptr<MyClass, decltype(Release)> unique(&myObject, Release);
 
-//CYCLIC DEPENDENCIES
+// CYCLIC DEPENDENCIES
 class WeakClass;
 class SharedClass
 {
@@ -306,18 +314,18 @@ public:
 class WeakClass
 {
 public:
-    //using another shared pointer creates a cyclic dependency where
-    //the reference counter never reaches 0 and the object is not destroyed
+    // using another shared pointer creates a cyclic dependency where
+    // the reference counter never reaches 0 and the object is not destroyed
     std::shared_ptr<SharedClass> sp;
 
-    //using a weakpointer prevents this
+    // using a weakpointer prevents this
     std::weak_ptr<SharedClass> wp;
 };
 
-//SHARED THIS
-//Used when storing this pointer both internally and externally as shared_ptr to prevent double deletion
-//Looks up control block for object in first shared_ptr, throws exception if one not found
-//Uses Curiously recurring template pattern (CRTP)
+// SHARED THIS
+// Used when storing this pointer both internally and externally as shared_ptr to prevent double deletion
+// Looks up control block for object in first shared_ptr, throws exception if one not found
+// Uses Curiously recurring template pattern (CRTP)
 class MyClass : public std::enable_shared_from_this<MyClass>
 {
     std::vector<std::shared_ptr<MyClass>> sm_vector;
