c4f1ea436ff8c71c929756b75e005959820b3775
Kara Jensen <mail@karajensen.com>
2024-10-28 13:49:31 +0100

    Update TypeScript.ts

--- Languages/TypeScript.ts
+++ Languages/TypeScript.ts
@@ -2,22 +2,32 @@
 // TYPESCRIPT
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*************************************************************************************************************
+• Adds type system to Javascript and future features
 • Node.js is the environment used to run the TypeScript compiler
 • Use VSCode and Live Server for hotloading
 • Use NVM for Windows Node.js and NPM management
 **************************************************************************************************************/
 
+let x: string = 'str';        // Define type of variable
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// OBJECTS
+// Define types of an object
+interface MyObj {
+    str: string,
+    value: number
+};
+
 // FUNCTIONS
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // Allow specifying function type
-function add(x: number, y: number) {
+function fn(x: number, y: number) {
    return x + y;
 }
-
-
+function fn() : MyObj {
+  return {
+    str: "str",
+    value: 1.0
+  };
+}
 
 
 
