1743a3a6f1dc470e6651f64bd4cd89989c96bd19
Kara Jensen <mail@karajensen.com>
2024-10-28 16:02:03 +0100

    Update TypeScript.ts

--- Languages/TypeScript.ts
+++ Languages/TypeScript.ts
@@ -10,6 +10,7 @@
 
 // VARIABLES
 let x: string;                     // Define type of variable  
+let x: 'str';                      // String literal, can only hold value 'str'
 let x: boolean = true;
 let x: number = 100;               // All numbers are floating or bigint
 const x: number = "str";           // BAD Assigning string to number gives error (accepted in Javascript)
@@ -19,12 +20,30 @@ let x = 10;                        // Auto adds the ': number' with first assign
 let x: unknown;                    // Type not initially known but still requires type checking
 (x as number[]).length             // Requires calling methods with explicit type
 
+// ANY
+let x: any;                        // Type not known and disable type checking (Javascript)
+let x;                             // No type or assignment auto becomes 'any'
+
 // ARRAYS
 let x: string[] = ['x', 'y']; 
 let x = [1, 2, 3, 'x'];            // Type set to array of numbers and strings only
 
+// TUPLES
+let x: [string, number, number?];  // Tuple with 2 arguments and 1 optional argument
+x = ['str', 5];
+
+// UNIONS
+let x: number | string;            // Allow both number of string but no other type
+
+// ENUMS
+enum MyEnum {
+    One = 1,    // Normally starts with 0
+    Two         // Auto assigned 2
+}
+MyEnum.One
+
 // OBJECTS
-let obj: {};                       // Empty object
+let obj: {};   // Empty object
 let obj: {
     str: string;
     value: number;
@@ -37,10 +56,12 @@ interface MyObj {
 };
 
 // FUNCTIONS
-// Allow specifying function type
-function fn(x: number, y: number) {
-   return x + y;
-}
+function fn(x: number, y: number) : number { return x + y; } 
+function fn(x: number | string) { }      // Union as type
+function fn(x: MyEnum) { }               // Enum as type
+function fn(x: number, y?: number) { }   // Optional parameter, if not set will be undefined, must be last in list
+function fn(x: number = 10) { }          // Default values, can be set in any order in list
+function fn() : void { }                 // Void for no return, implicit
 function fn() : MyObj {
   return {
     str: "str",
@@ -52,7 +73,18 @@ function fn() : MyObj {
 let fn : (x: string) => string;
 fn = function (x: string) { return "str"};
 
-// TUPLE
+// TYPE ALIAS
+type MyAlias = MyType;
+type MyObject = {  // Alias object
+  str: string;
+  x: number;
+};
+type MyConcat = MyObject & MyObject2;  // Combining types
+let myConcat: MyConcat = {
+    str: "str"
+    x: 10
+    x2: 20
+};
 
 
 
